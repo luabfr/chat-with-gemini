14fc6da3ecac8f100109d1fac01e138a57c74620
luabfr <luabfr@gmail.com>
2026-06-17 07:21:24 -0300

    integracion de mercadopago checkout Pro. fix proxy auth

--- proxy.ts
+++ proxy.ts
@@ -52,8 +52,11 @@ const RUTAS_PROTEGIDAS = ["/cuenta","/checkout"]
 }
 
 export const config = {
+	// matcher: [
+	// 	"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
+	// ]
 	matcher: [
-		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
+		"/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
 	]
 }
 
